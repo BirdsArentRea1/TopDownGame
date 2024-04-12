c8021ff79f76b331156993397955476f0c87d633
BirdsArentRea1 <146360250+BirdsArentRea1@users.noreply.github.com>
2024-04-12 11:08:06 -0600

    Update player.py

--- player.py
+++ player.py
@@ -7,8 +7,8 @@ DOWN = 3
 SPACE = 4
 W = 5
 
-Chicken = pygame.image.load('chicken.png') 
-Chicken.set_colorkey((255, 0, 255)) 
+wizard = pygame.image.load('SpriteSheetWizard.png') 
+wizard.set_colorkey((255, 0, 255)) 
 
 class player:
     def __init__(self):
@@ -19,43 +19,49 @@ class player:
         self.vy = 0
         self.direction = LEFT
         #animation variables variables
-        self.frameWidth = 69
-        self.frameHeight = 69
+        self.frameWidth = 100
+        self.frameHeight = 100
         self.RowNum = 2 
         self.frameNum = 0
         self.ticker = 0
         
 
     def draw(self,screen):
-        pygame.draw.rect(screen, (255, 0, 255), (self.xpos, self.ypos, 30, 30))
-        screen.blit(Chicken, (self.xpos, self.ypos), (self.frameWidth*self.frameNum, self.RowNum*self.frameHeight, self.frameWidth, self.frameHeight)) 
+        #pygame.draw.rect(screen, (255, 0, 255), (self.xpos, self.ypos, 30, 30))
+
+        screen.blit(wizard, (self.xpos, self.ypos), (self.frameWidth, self.RowNum*self.frameHeight, self.frameWidth, self.frameHeight)) 
     def move(self, keys, map):
+
+        #movement algorithm--------------------------
         if keys[LEFT] == True:
             self.vx = -3
-            self.RowNum = 2
+            self.RowNum = 0
             self.direction = LEFT
             print("left")
         elif keys[RIGHT] == True:
             self.vx = 3
-            self.RowNum = 3
+            self.RowNum = 1
             self.direction = RIGHT
             print("right")
         
 
         else:
             self.vx = 0
+
         if keys[UP] == True:
             self.vy = -3
-            self.RowNum = 0
+            self.RowNum = 3
             self.direction = UP
             print("up")
         elif keys[DOWN] == True:
             self.vy = 3
-            self.RowNum = 1
+            self.RowNum = 2
             self.direction = DOWN
             print("down")
         else:
             self.vy = 0
+
+        #collision------------------------------
         if map[int((self.ypos- 10) / 50 )][int((self.xpos - 10) / 50)] == 2:
             self.xpos+=3
 
@@ -68,6 +74,7 @@ class player:
         if map[int((self.ypos+ 30 + 5) / 50 )][int((self.xpos ) / 50)] == 2:
             self.ypos-=3
  
+        #update posistion
         self.xpos+=self.vx
         self.ypos+=self.vy
 
